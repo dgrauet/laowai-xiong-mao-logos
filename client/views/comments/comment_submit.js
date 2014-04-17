4370c432255dabb4ca2e1b53b8ad48f280871c08
Damien Grauet <damien.grauet@gmail.com>
2014-04-17 19:42:31 +0200

    ajout collectionFS + graphicsmagick + upload
    début gestion upload images dans commentaires + installation de paquets

--- client/views/comments/comment_submit.js
+++ client/views/comments/comment_submit.js
@@ -1,7 +1,14 @@
 Template.commentSubmit.events({
-  'submit form': function(e, template) {
-    e.preventDefault();
-    var $body = $(e.target).find('[name=body]');
+  'change #input-files': function(event, template) {
+    FS.Utility.eachFile(event, function(file) {
+      Files.insert(file, function (err, fileObj) {
+        //Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+      });
+    });
+  },
+  'submit form': function(event, template) {
+    event.preventDefault();
+    var $body = $(event.target).find('[name=body]');
     var comment = {
       body: $body.val(),
       ticketId: template.data._id
