63a511d016d05fe488a4388924d5d737b27569f9
Damien Grauet <damien.grauet@gmail.com>
2014-07-07 14:11:00 +0200

    Clean Server Side reference + UX on insert
    - Deleted references insert on server-side collection
    - Input cleaning after insert

--- collections/references.js
+++ collections/references.js
@@ -4,28 +4,3 @@ References = new Meteor.Collection(null);
 clearReferences = function() {
   References.remove({});
 }
-
-Meteor.methods({
-  reference: function(referenceAttributes) {
-    var user = Meteor.user();
-
-    // ensure the user is logged in
-    if (!user){
-      throw new Meteor.Error(401, 'Vous devez vous connecter pour insérer un numéro de référence');
-    }
-    
-    if (!referenceAttributes.number){
-      throw new Meteor.Error(403, 'Veuillez renseigner un numéro de ticket');
-    }
-
-    var reference = _.extend(_.pick(referenceAttributes, 'number'), {
-      number: referenceAttributes.number
-    });
-
-    var referenceId = References.insert({
-      number: reference.number
-    });
-    return referenceId;
-    
-  }
-});
