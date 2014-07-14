8dbdb03c5b986e2c7b01bba522e70c6cd639d7f0
Damien Grauet <damien.grauet@gmail.com>
2014-07-14 16:48:15 +0200

    Transformed attachment UX
    Turned drop div into button
    Attached drop button to hidden input file

--- client/views/comments/comment_submit.js
+++ client/views/comments/comment_submit.js
@@ -1,4 +1,13 @@
 Template.commentSubmit.events({
+  'click #fileBtn': function() {
+    document.querySelector("#fileInput").click();
+  },
+  'change #fileInput': function(event){
+    event.preventDefault();
+    reader = new FileReader();
+    var files = document.querySelector("#fileInput").files;
+    console.log(files);
+  },
   'submit form': function(event, template) {
     event.preventDefault();
     var $body = $(event.target).find('[name=body]');
