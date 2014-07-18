d25fe3e8e40681e6bc00ff1b42dbdeab14607993
Damien Grauet <damien.grauet@gmail.com>
2014-07-18 08:02:39 +0200

    - Ajout npm - Upload fonctionnel en mode light

--- client/views/file/file_input.js
+++ client/views/file/file_input.js
@@ -1,13 +1,35 @@
 Template.fileInput.events({
   'click #fileBtn': function() {
-    document.querySelector("#fileInput").click();
+    document.getElementById("fileInput").click();
   },
   'change #fileInput': function(event, template){
     event.preventDefault();
-    FS.Utility.eachFile(event, function(file) {
-      Images.insert(file, function (err, fileObj) {
-        if(err) throw err;
-      });
+    _.each(event.currentTarget.files, function(file) {
+      Meteor.saveFile(file, file.name);
     });
   }
 });
+
+Meteor.saveFile = function(blob, name, path, type, callback) {
+  var fileReader = new FileReader(),
+    method, encoding = 'binary', type = type || 'binary';
+  switch (type) {
+    case 'text':
+    // TODO Is this needed? If we're uploading content from file, yes, but if it's from an input/textarea I think not...
+    method = 'readAsText';
+    encoding = 'utf8';
+    break;
+    case 'binary':
+    method = 'readAsBinaryString';
+    encoding = 'binary';
+    break;
+    default:
+    method = 'readAsBinaryString';
+    encoding = 'binary';
+    break;
+  }
+  fileReader.onload = function(load) {
+    Meteor.call('saveFile', load.target.result, name, path, encoding, callback);
+  }
+  fileReader[method](new Blob([blob]));
+}
