18eb564d64cb1f46e9147ab5a29c8d96fb0f39ad
Damien Grauet <damien.grauet@gmail.com>
2014-08-03 00:29:05 +0200

    drop + cleaning

--- client/views/file/file_submit.js
+++ client/views/file/file_submit.js
@@ -2,7 +2,7 @@ Template.fileSubmit.events({
   'click #fileBtn': function() {
     document.getElementById("fileInput").click();
   },
-  'change #fileInput': function(event, template){
+  'change #fileInput': function(event, template) {
     event.preventDefault();
     FS.Utility.eachFile(event, function(file) {
       var fileObj = new FS.File(file);
@@ -17,7 +17,36 @@ Template.fileSubmit.events({
         } else {
           Tickets.update( fileObj.metadata.ticketId, {
             $inc: { attachmentsCount: 1 },
-            $addToSet: { attachments: fileObj.original._id }
+            $addToSet: { attachments: fileObj.original.name }
+          }, function(err, count){
+            if (err) {
+              console.log( err.reason );
+            } else {
+              console.log(count + " records made");
+            }
+          });
+        }
+      //If !err, we have inserted new doc with ID fileObj._id, and
+      //kicked off the data upload using HTTP
+      });
+    });
+  },
+  'dropped #dropzone': function(event, template) {
+    event.preventDefault();
+    FS.Utility.eachFile(event, function(file) {
+      var fileObj = new FS.File(file);
+      fileObj.metadata = {
+        owner: Meteor.userId(),
+        ticketId: template.data._id
+      };
+      console.log(fileObj);
+      Images.insert(fileObj, function (err, fileObj) {
+        if (err){
+          console.log(err.reason);
+        } else {
+          Tickets.update( fileObj.metadata.ticketId, {
+            $inc: { attachmentsCount: 1 },
+            $addToSet: { attachments: fileObj.original.name }
           }, function(err, count){
             if (err) {
               console.log(err.reason);
@@ -25,10 +54,6 @@ Template.fileSubmit.events({
               console.log(count + " records made");
             }
           });
-          // Meteor.call('addAttachment', fileObj, function(err, id){
-          //   if(err)
-          //     throwError(err.reason);
-          // });
         }
       //If !err, we have inserted new doc with ID fileObj._id, and
       //kicked off the data upload using HTTP
--- collections/images.js
+++ collections/images.js
@@ -3,13 +3,13 @@ Images = new FS.Collection("images", {
     new FS.Store.FileSystem("images", {
       path: "~/uploads/full",
       beforeWrite: function(fileObj) {
-        console.log("normal : " + fileObj.metadata);
+
       }
     }),
     new FS.Store.FileSystem("thumbs", {
       path: "~/uploads/thumbs",
       beforeWrite: function (fileObj) {
-        console.log("thumbs : " + fileObj)
+
       },
       transformWrite: function(fileObj, readStream, writeStream) {
         // Transform the image into a 10x10px thumbnail
@@ -29,21 +29,4 @@ Images.allow({
   update: ownsDocument,
   remove: ownsDocument,
   download: function(){ return true; }
-});
-
-Meteor.methods({
-  addAttachment: function(fileObj) {
-    console.log("fileObj : " + fileObj);
-    console.log("ticketId : " + fileObj.metadata.ticketId);
-    Tickets.update( fileObj.metadata.ticketId, {
-      $inc: { attachmentsCount: 1 },
-      $addToSet: { attachments: fileObj.original._id }
-    }, function(err, count){
-      if (err) {
-        console.log(err.reason);
-      } else {
-        console.log(count + " records made");
-      }
-    });
-  }
 });
